a85750c4165a2911c35bda32d66f76d761cafe18
Sajib Kundu <sajibkundu.swe@gmail.com>
2025-08-09 13:34:58 +0600

    Create validation.h

---
+++ validation.h
@@ -0,0 +1,9 @@
+#ifndef VALIDATION_H
+#define VALIDATION_H
+int validate_email(const char *email);
+int validate_phone(const char *phone);
+int validate_nid(const char *nid);
+int validate_datetime(const char *dt);
+int validate_plate(const char *plate);
+int validate_password(const char *pw);
+#endif
